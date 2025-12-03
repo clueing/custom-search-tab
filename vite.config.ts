44a33af650ff755632dbedec1e007d2ac38ffc04
clue <clueing@outlook.com>
2025-12-03 10:34:21 +0800

    feat: 使用 CRXJS 和 Vite 初始化 Vue 3 项目

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from 'node:path'
+import { crx } from '@crxjs/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack'
+import manifest from './manifest.config.ts'
+import { name, version } from './package.json'
+import UnoCSS from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
+  plugins: [
+    vue(),
+    UnoCSS(),
+    crx({ manifest }),
+    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
+  ],
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+})
